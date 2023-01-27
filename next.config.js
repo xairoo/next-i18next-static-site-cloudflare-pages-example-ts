a639670ef9f7045b36ac3ce454eb2c2eb49cf0aa
Xairoo <marcel.3ff8@zurreck.com>
2023-01-27 21:46:26 +0100

    web-ts example added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  publicRuntimeConfig: {
+    i18n: {
+      languages: ["en", "de"],
+      defaultLanguage: "en",
+      namespaces: ["common", "meta", "error"],
+      defaultNamespace: "common",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import {
+  I18nProvider,
+  languages,
+  defaultLanguage,
+  namespaces,
+  defaultNamespace,
+} from "next-i18next-static-site";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// Locales loader
+import locales from "../lib/locales";
+
+import Header from "../components/header";
+import "./styles.css";
+
+const App = function ({ Component, pageProps }: AppProps) {
+  // i18n options
+  const i18n = {
+    languages,
+    defaultLanguage,
+    namespaces,
+    defaultNamespace,
+    locales,
+  };
+
+  return (
+    <I18nProvider i18n={i18n} /* Pass the i18n options to the i18n provider */>
+      <Header />
+      <Component {...pageProps} />
+    </I18nProvider>
+  );
+};
+
+export default App;
